b2fe3bacc97fec0b4ca3b30370838e90383be0dd
aljongjong <aljongjong@gmail.com>
2023-01-10 15:38:42 +0900

    20230110_01 upload

---
+++ src/main/java/aljongjong/aol/AolApplication.java
@@ -0,0 +1,13 @@
+package aljongjong.aol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aljongjong/aol/AolApplicationTests.java
@@ -0,0 +1,13 @@
+package aljongjong.aol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
